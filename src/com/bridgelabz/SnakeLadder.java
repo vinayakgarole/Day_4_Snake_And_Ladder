3193ce43b8d154f407b248b6de6698f1b7544eb1
vinayakgarole <vinayakgarole10@gamil.com>
2023-01-20 13:28:56 +0530

    Added: Player at stepcount zero

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Snake and Ladder Program");
+        System.out.println("Starting of Snake and Ladder Game");
+        System.out.println("The single player is at position 0");
+        System.out.println("----------------------------------");
     }
 }
